f798d586318e2f7b3eece83e97a22efca0a1a632
Froggi22 <daniel.heinstedt@gmail.com>
2022-01-30 00:50:08 +0100

    Nav & Footer klart
    Lagt till vscode workspace settings.
    Nya bilder, inkl Favicon och namn.
    Parallax bild.
    Uppdaterat README.
    Media rules.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Greenscan
+
+Greenscan är en idé, som försöker lösa en av dem globala mål som Hack Your World 2022 har tagit fram.
--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function submitForm() {
+/* Todo
+	Maybe something after entering search query
+*/
 
-}
-
+/*===== Adding submit event listener to nav-form =====*/
 document.getElementById("navForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("lol ;)");
+    event.preventDefault(); // Prevents website to reload on submit
+	query = document.getElementById("search").value.trim() // Fetch search query
+	if (query !== "") { // If query isn't only whitespace
+		alert(`Du har sökt efter: ${document.getElementById("search").value}`); // Prompting the search query
+	}
 })
+
+/*===== To Top Arrow =====*/
+toTopArrow = document.getElementById("to-top-arrow"); // Fetch "button"
+window.onscroll = function() { scrollFunction() }; // Call scrollFunction on scroll event
+
+function scrollFunction() {
+	if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) { // If user has scrolled past 30px
+		toTopArrow.style.visibility = "visible"; // Make button visible
+		toTopArrow.style.opacity = 1; // Visible transition
+		toTopArrow.style.transition = "visibility 0s, opacity 0.5s ease-in-out"; // Actual transition
+	} else {
+		toTopArrow.style.visibility = "hidden"; // Make button hidden
+		toTopArrow.style.opacity = 0; // Hidden transition
+		toTopArrow.style.transition = "visibility 0.5s, opacity 0.5s ease-in-out"; // Actual transition
+	}
+}
